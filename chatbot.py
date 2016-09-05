786e2cc7af156e0870bdc75e9a6bbfb1e5af9464
Shubhankar Deshpande <shubhankardeshpande@hotmail.com>
2016-09-05 08:07:52 +0000

    Add brain file to speed startup

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,13 @@
 import aiml
+import os
 
 #Create the kernel and learn the AIML files
 kernel = aiml.Kernel()
-kernel.learn("std-startup.xml")
-kernel.respond("load aiml b")
+if os.path.isfile("bot_brain.brn"):
+	kernel.bootstrap(brainFile = "bot_brain.brn")
+else:
+	kernel.bootstrap(learnFiles = "std-startup.xml", commands="load aiml b")
+	kernel.saveBrain("bot_brain.brn")
 
 #Press CTRL-C to break the loop
 while True:
